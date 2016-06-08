9d4ec24bf56cc0def67495bd6fc17d90e1cdd9bd
Gregory Knudsen <dev.artist15@gmail.com>
2016-06-08 11:54:32 -0400

    write first component with some JSX basics

--- 01 - Introduction - Start Here/scripts/main.js
+++ 01 - Introduction - Start Here/scripts/main.js
@@ -9,8 +9,14 @@ var ReactDOM = require('react-dom');
 var StorePicker = React.createClass({
 
 	render : function() {
+		var name = 'greg';
 		return (
-			<p>hello</p>
+			<form className="store-selector">
+		{/* How to write a comment in JSX */}
+				<h2>Please Enter a Store</h2>
+				<input type="text" ref="storeId" required />
+				<input type="Submit" />
+			</form>
 		)
 	}
 });
