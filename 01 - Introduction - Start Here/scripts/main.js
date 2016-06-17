4fead45b9bbd0a7873b963a3ed96f61472cb0c7e
Gregory Knudsen <dev.artist15@gmail.com>
2016-06-17 12:29:09 -0400

    addFish form added as well as referencing fish object

--- 01 - Introduction - Start Here/scripts/main.js
+++ 01 - Introduction - Start Here/scripts/main.js
@@ -35,6 +35,22 @@ var App = React.createClass({
 */
 
 var AddFishForm = React.createClass({
+	createFish: function(event) {
+		// 1. Stop form from submitting
+		event.preventDefault();
+		// 2. Take data from the form and create object
+		var fish = {
+			name : this.refs.name.value,
+			price : this.refs.price.value,
+			status : this.refs.status.value,
+			desc : this.refs.desc.value,
+			image : this.refs.image.value
+		}
+
+		// 3. Add the fish to the App State
+
+
+	},
 	render: function() {
 		return (
       <form className="fish-edit" ref="fishForm" onSubmit={this.createFish}>
